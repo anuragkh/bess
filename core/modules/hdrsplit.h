ddc80942cc2ef9cc7303328b724021a68d05fe4f
Anurag Khandelwal <anuragk@berkeley.edu>
2016-11-07 13:55:33 -0800

    Added header split module

---
+++ core/modules/hdrsplit.h
@@ -0,0 +1,17 @@
+#include "../module.h"
+
+class HeaderSplit : public Module {
+ public:
+  virtual void ProcessBatch(struct pkt_batch *batch);
+
+  static const gate_idx_t kNumIGates = 1;
+  static const gate_idx_t kNumOGates = 2;
+  
+  static const gate_idx_t kRegOut = 0;
+  static const gate_idx_t kHdrOut = 1;
+  
+  static const size_t kMaxHdrSize = 138;
+
+  static const Commands<Module> cmds;
+  static const PbCommands<Module> pb_cmds;
+};
